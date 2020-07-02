483510f80711485b4f4a411303f106b32a3de7f3
Blake Aghili <aghili.babak@gmail.com>
2020-07-02 18:20:16 -0500

    Before Going Home

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from '../components/Header'
 import Meta from "./Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+    background: red;
+    font-size = 100px;
+`;
 
 class Page extends React.Component {
     render() {
@@ -8,10 +14,10 @@ class Page extends React.Component {
             <div>
                 <Meta/>
                 <Header/>
+                <MyButton>Click Me</MyButton>
                 {this.props.children}
             </div>
         )
-
     }
 }
 
